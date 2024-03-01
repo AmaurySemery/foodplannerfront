986471be340d8aa17cf2711a8634d68b51d428a5
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-01 19:25:27 +0100

    Ajout de la navigation

--- src/assets/Layout.tsx
+++ src/assets/Layout.tsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom"
+
 function Layout() {
-  return (
-    <div>Layout</div>
-  )
+    return (
+        <>
+            <nav className="navbar">
+                <NavLink to='/home'>Home</NavLink>
+                <NavLink to='/login'>Login</NavLink>
+            </nav>
+        </>
+    )
 }
 
 export default Layout
