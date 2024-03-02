9d807b2e68923d1d55dcce6a43e0c00ba86f0f97
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-02 16:58:54 +0100

    Connecter le front FormRegister au back strapi

--- src/components/FormRegister.tsx
+++ src/components/FormRegister.tsx
@@ -1,4 +1,4 @@
-import { Form, Link } from "react-router-dom"
+import { ActionFunctionArgs, Form, Link } from "react-router-dom"
 
 function FormRegister() {
     return (
@@ -17,6 +17,36 @@ function FormRegister() {
             <Link to='/login'>Login (already have an account)</Link>
         </>
     )
+}
+
+export const registerAction = async ({ request }: ActionFunctionArgs) => {
+    const data = await request.formData()
+    const username = data.get('username')
+    const email = data.get('email')
+    const password = data.get('password')
+    const confirmpass = data.get('confirmpass')
+
+    const strapiRegisterUrl = 'http://localhost:1337/api/auth/local/register'
+
+    const registerData = {
+        email,
+        password,
+        username
+    }
+
+    const register = await fetch(strapiRegisterUrl, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(registerData, null, 2)
+    })
+
+    const registerResponseData = await register.json()
+    console.log({ registerResponseData })
+
+    return null
 }
 
 export default FormRegister
