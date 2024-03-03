32c23530b2212ac7e3ed2f9d25e19f26d32f5a6f
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-03 17:29:42 +0100

    Afficher l'api de foodlist dans strap sur la page Food dans React

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react"
+
 const Page = () => {
+  // Le ?populate=* permet d'accéder à la jointure
+  const foodListUrl = 'http://localhost:1337/api/foodlist?populate=*'
+  const [foodList, setFoodList] = useState(null)
+  useEffect(() => {
+    const getFoodList = async () => {
+      const res = await fetch(foodListUrl)
+      const jsonData = await res.json()
+      console.log({jsonData})
+      setFoodList(jsonData)
+    }
+    getFoodList()
+  }, [])
+
   return (
-    <div>Food</div>
+    <>
+    <h3>Food</h3>
+    {JSON.stringify(foodList)}
+    </>
   )
 }
 
