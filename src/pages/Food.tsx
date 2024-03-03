f44796abb80dcec090641eca27b72b9f2880a259
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-03 17:44:14 +0100

    Rendre plus intelligible l'affichage de la requête API dans Food

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react"
+import FoodItem from "../interfaces/FoodItem"
 
 const Page = () => {
   // Le ?populate=* permet d'accéder à la jointure
   const foodListUrl = 'http://localhost:1337/api/foodlist?populate=*'
-  const [foodList, setFoodList] = useState(null)
+  const [foodList, setFoodList] = useState<FoodItem[]>([])
   useEffect(() => {
     const getFoodList = async () => {
       const res = await fetch(foodListUrl)
       const jsonData = await res.json()
       console.log({jsonData})
-      setFoodList(jsonData)
+      const parsedResult = foodItemsFormJson(jsonData.data)
+      setFoodList(parsedResult)
     }
     getFoodList()
   }, [])
+
+  function foodItemsFormJson(data: any[]): FoodItem[] {
+    return data.map(item => {
+      const foodItem: FoodItem = {
+        id: item.id,
+        name: item.attributes.Name,
+        dateAdded: new Date(item.attributes.DateAdded),
+        foodCategory: item.attributes["food_category"].data.attributes.name,
+        maxStayInFreezerInMonth: item.attributes["food_category"].data.attributes.MaxStayInFreezer
+
+      }
+      return foodItem
+    })
+  }
 
   return (
     <>
