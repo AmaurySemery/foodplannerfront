0112b26c13ec94233fb6a58c18601ed2548c9078
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-04 17:02:46 +0100

    Meilleur format de date dans FoodCard

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -6,10 +6,14 @@ interface Props {
 }
 
 function FoodCard({ item }: Props) {
+  function niceDate(date: Date): string {
+    return date.toISOString().split('T')[0]
+  }
+
   return (
     <div className={styles.card}>
         <h3>{item.name}</h3>
-        <div>Date added: {item.dateAdded.toISOString()}</div>
+        <div>Date added: {niceDate(item.dateAdded)}</div>
         <div>Max stay in freezer: {item.maxStayInFreezerInMonth} months</div>
         <div>Category: {item.foodCategory}</div>
     </div>
