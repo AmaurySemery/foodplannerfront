e64e1525ed43a8350cf928c071f21327eebdc940
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-05 13:34:30 +0100

    Confirmation de suppression

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,22 +1,42 @@
+import { useState } from "react"
 import { add } from "date-fns"
 import FoodItem from "../interfaces/FoodItem"
 import styles from './fooditem.module.css'
 
+import 'react-responsive-modal/styles.css'
+import Modal from "react-responsive-modal"
+
 interface Props {
-    item: FoodItem
+  item: FoodItem
 }
 
 function FoodCard({ item }: Props) {
+  const [open, setOpen] = useState(false)
+
+  const onOpenModal = () => setOpen(true)
+  const onCloseModal = () => setOpen(false)
+
   function niceDate(date: Date): string {
     return date.toISOString().split('T')[0]
   }
 
   return (
     <div className={styles.card}>
-        <h3>{item.name}</h3>
-        <div>Date added: {niceDate(item.dateAdded)}</div>
-        <div>Max stay in freezer: {niceDate(add(item.dateAdded, {months : item.maxStayInFreezerInMonth}))}</div>
-        <div>Category: {item.foodCategory}</div>
+      <h3>{item.name}</h3>
+      <div>Date added: {niceDate(item.dateAdded)}</div>
+      <div>Max stay in freezer: {niceDate(add(item.dateAdded, { months: item.maxStayInFreezerInMonth }))}</div>
+      <div>Category: {item.foodCategory}</div>
+      <br />
+      <div>
+        <button onClick={onOpenModal}>Delete</button>
+        <Modal open={open} onClose={onCloseModal} center>
+          <h2>Delete {item.name} ?</h2>
+          <p>
+            Are you sure to delete {item.name} ?
+          </p>
+        </Modal>
+      </div>
+
     </div>
   )
 }
