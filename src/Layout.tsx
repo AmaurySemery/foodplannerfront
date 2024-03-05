6eedc567609a4a7bc8fc2ed12e08d9abc79212b0
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-05 13:12:23 +0100

    On n'oublie pas l'espace pour que la classe selected soit prise en compte

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,11 +4,11 @@ function Layout() {
     return (
         <>
             <nav className="navbar">
-                <NavLink to='/home' className={({isActive}) => "nav-link" + (isActive ? "selected": "")}>Home</NavLink>{'  '}
-                <NavLink to='/login' className={({isActive}) => "nav-link" + (isActive ? "selected": "")}>Login</NavLink>{'  '}
-                <NavLink to='/register' className={({isActive}) => "nav-link" + (isActive ? "selected": "")}>Register</NavLink>{'  '}
-                <NavLink to='/foodlist' className={({isActive}) => "nav-link" + (isActive ? "selected": "")}>Food</NavLink>{'  '}
-                <NavLink to='/foodcreate' className={({isActive}) => "nav-link" + (isActive ? "selected": "")}>Add Food</NavLink>{'  '}
+                <NavLink to='/home' className={({isActive}) => "nav-link" + (isActive ? " selected": "")}>Home</NavLink>{'  '}
+                <NavLink to='/login' className={({isActive}) => "nav-link" + (isActive ? " selected": "")}>Login</NavLink>{'  '}
+                <NavLink to='/register' className={({isActive}) => "nav-link" + (isActive ? " selected": "")}>Register</NavLink>{'  '}
+                <NavLink to='/foodlist' className={({isActive}) => "nav-link" + (isActive ? " selected": "")}>Food</NavLink>{'  '}
+                <NavLink to='/foodcreate' className={({isActive}) => "nav-link" + (isActive ? " selected": "")}>Add Food</NavLink>{'  '}
             </nav>
             <div className="main">
                 <Outlet />
