e17eb4c292b2c5e5f331afb70be9078cf81e243c
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-06 14:17:41 +0100

    On ajoute un espace en attendant

--- src/components/OrderBy.tsx
+++ src/components/OrderBy.tsx
@@ -5,7 +5,7 @@ function OrderBy() {
         <>
             <h4>OrderBy</h4>
             <div>
-                Name
+                Name{' '}
                 <label className={styles.switch}>
                     <input type="checkbox" />
                     <span className={`${styles.slider} ${styles.round}`}></span>
