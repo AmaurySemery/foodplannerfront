c9e8042cca65a44df45e21397137102b56b92f62
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-06 14:09:21 +0100

    Préparation du HTML dans OrderBy

--- src/components/OrderBy.tsx
+++ src/components/OrderBy.tsx
@@ -1,7 +1,17 @@
 function OrderBy() {
-  return (
-    <div>OrderBy</div>
-  )
+    return (
+        <>
+            <h4>OrderBy</h4>
+            <div>
+                Name
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider"></span>
+                </label>
+            </div>
+        </>
+
+    )
 }
 
 export default OrderBy
