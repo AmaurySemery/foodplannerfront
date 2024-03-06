6f5a6146ace9ec63dfc4606b0e4bed5c24ac1db1
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-06 14:20:01 +0100

    On améliore le style et on ajoute un nouveau switch

--- src/components/OrderBy.tsx
+++ src/components/OrderBy.tsx
@@ -4,8 +4,15 @@ function OrderBy() {
     return (
         <>
             <h4>OrderBy</h4>
-            <div>
-                Name{' '}
+            <div className={styles.center}>
+                Name
+                <label className={styles.switch}>
+                    <input type="checkbox" />
+                    <span className={`${styles.slider} ${styles.round}`}></span>
+                </label>
+            </div>
+            <div className={styles.center}>
+                Max date
                 <label className={styles.switch}>
                     <input type="checkbox" />
                     <span className={`${styles.slider} ${styles.round}`}></span>
