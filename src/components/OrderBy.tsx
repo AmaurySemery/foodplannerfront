13fb5cddc186dc6dcb7b4ffdb2884b4f20809b20
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-06 14:36:14 +0100

    On intègre la fonction de Food via une fonction intermédiaire pour le switch toggle

--- src/components/OrderBy.tsx
+++ src/components/OrderBy.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent } from 'react';
 import styles from './orderby.module.css'
 
 interface Props {
@@ -5,13 +6,23 @@ interface Props {
 }
 
 function OrderBy({orderItem}: Props) {
+    function handleNameChange(e: ChangeEvent & { target: HTMLInputElement }) {
+        console.log(e.target.checked)
+        const isChecked = e.target.checked
+        if (isChecked) {
+            orderItem('name')
+        } else {
+            orderItem('nameoff')
+        }
+    }
+
     return (
         <>
             <h4>OrderBy</h4>
             <div className={styles.center}>
                 Name
                 <label className={styles.switch}>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => handleNameChange(e)} />
                     <span className={`${styles.slider} ${styles.round}`}></span>
                 </label>
             </div>
